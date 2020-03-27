a7660c6584f1b6bcbd0c29e344ee2f2421956bf7
dnagafonov <denisagafonovwork@gmail.com>
2020-03-27 23:48:58 +0300

    refactor: add sass

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Header from '../header/header';
+import ListGood from '../list-good/list-good';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <main className="main">
+        {[{ name: "Notebook", price: 1000 }, { name: "Laptop", price: 2000 }].map(el => <ListGood name={el.name} price={el.price} />)}
+      </main>
     </div>
   );
 }
---
+++ src/components/list-good/list-good.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './list-good.scss'
+
+const ListGood = props => {
+    const { name, price } = props;
+    return (
+        <div className="list-good">
+            <div className="list-good__image">
+                <img src="" alt="" />
+            </div>
+            <div className="list-good__description">
+                <div className="list-good__name">{name}</div>
+                <div className="list-good__price">{price}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ListGood;
