30f3d2a979ee4edfaf1a82a3e2c3fa722049ef51
dnotebook <denisagafonovwork@gmail.com>
2020-04-06 02:21:08 +0300

    fix: complete welcome component && add smooth scroll

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './welcome.scss'
 import { Link } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
 
 const Welcome = () => {
     return (
@@ -8,14 +9,19 @@ const Welcome = () => {
             <div className="wrapper welcome__wrapper">
                 <div className="welcome__header">
                     <h1>Welcome</h1>
-                    <a className="arrow-down" href="#down">
+                    <ScrollLink 
+                        className="arrow-down" 
+                        to="down"
+                        smooth={true}
+                        offset={-90}
+                    >
                         <i className="fas fa-angle-double-down"></i>
-                    </a>
+                    </ScrollLink>
                 </div>
                 <div className="welcome__layout" id="down">
-                    <Link to="/about-us">About us</Link>
-                    <Link to ="/spares">spares</Link>
-                    <Link to="/services">Services</Link>
+                    <Link to="/about-us"><h4>About us</h4></Link>
+                    <Link to="/spares"><h4>spares</h4></Link>
+                    <Link to="/services"><h4>Services</h4></Link>
                 </div>
             </div>
         </div>
